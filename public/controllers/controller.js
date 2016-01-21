cc9e232ad3ca6ae5938d43d9f65d3aa66a6890c2
colinmiller <colinchmiller@gmail.com>
2016-01-21 14:22:45 -0600

    Completed from online walkthrough

---
+++ public/controllers/controller.js
@@ -0,0 +1,48 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
+    console.log("Hello world from controller");
+
+    var refresh = function(){
+        $http.get('/contactlist').success(function(res){
+           console.log("Data received from request");
+            $scope.contactList = res;
+            $scope.contact = "";
+        });
+    };
+    refresh();
+
+    $scope.addContact = function(){
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function(res){
+            console.log(res);
+            refresh();
+        });
+    };
+
+    $scope.remove = function(id){
+        console.log(id);
+        $http.delete('/contactlist/' + id).success(function(res){
+           refresh();
+        });
+    };
+
+    $scope.edit = function(id){
+        console.log(id);
+        $http.get('/contactlist/' + id).success(function(res){
+            $scope.contact = res;
+        });
+    };
+
+    $scope.update = function(){
+        console.log($scope.contact._id);
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(res){
+            refresh();
+        });
+    };
+
+    $scope.deselect = function(){
+        $scope.contact = "";
+    };
+
+}]);
